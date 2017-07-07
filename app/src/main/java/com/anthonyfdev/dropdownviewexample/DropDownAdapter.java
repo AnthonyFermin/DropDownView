965c57fd388dd00ed79ef26a77df79fc178081e0
Anthony Fermin <afermin@fuzzproductions.com>
2017-07-07 14:52:51 -0400

    Initial Commit

---
+++ app/src/main/java/com/anthonyfdev/dropdownviewexample/DropDownAdapter.java
@@ -0,0 +1,80 @@
+package com.anthonyfdev.dropdownviewexample;
+
+/**
+ * Created by afermin on 7/6/17.
+ */
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author Anthony Fermin (Fuzz)
+ */
+public class DropDownAdapter extends RecyclerView.Adapter<DropDownAdapter.StandViewHolder> {
+
+    private final ViewActions actionViewDelegate;
+
+    public DropDownAdapter(final ViewActions actionViewDelegate) {
+        this.actionViewDelegate = actionViewDelegate;
+    }
+
+    @Override
+    public StandViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_stand_drop_down, parent, false);
+        return new StandViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(StandViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return MainActivity.NUM_OF_STANDS;
+    }
+
+    class StandViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView standTitleTV;
+        private final TextView standStatusTV;
+
+        public StandViewHolder(View itemView) {
+            super(itemView);
+            standTitleTV = itemView.findViewById(R.id.cell_stand_title);
+            standStatusTV = itemView.findViewById(R.id.cell_stand_status);
+            itemView.setBackgroundDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.stand_drop_down_selector));
+            itemView.setOnClickListener(standViewItemClickListener);
+        }
+
+        public void bind(int position) {
+            standTitleTV.setText(actionViewDelegate.getStandTitle(position));
+            standStatusTV.setText(actionViewDelegate.getStandStatus(position));
+            itemView.setSelected(actionViewDelegate.getSelectedStand() == position);
+        }
+
+        private final View.OnClickListener standViewItemClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int lastSelectedPosition = actionViewDelegate.getSelectedStand();
+                actionViewDelegate.setSelectedStand(getAdapterPosition());
+                notifyItemChanged(lastSelectedPosition);
+                notifyItemChanged(getAdapterPosition());
+                actionViewDelegate.collapseDropDown();
+            }
+        };
+    }
+
+    interface ViewActions {
+        void collapseDropDown();
+        void setSelectedStand(int standId);
+        String getStandTitle(int standId);
+        String getStandStatus(int standId);
+        int getSelectedStand();
+    }
+}
+
---
+++ app/src/main/java/com/anthonyfdev/dropdownviewexample/MainActivity.java
@@ -0,0 +1,139 @@
+package com.anthonyfdev.dropdownviewexample;
+
+import android.animation.ObjectAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.anthonyfdev.dropdownview.DropDownView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int NUM_OF_STANDS = 4;
+    private RecyclerView recyclerView;
+    private DropDownAdapter adapter;
+    private int selectedStandId;
+    private String[] waitTimes;
+    private TextView selectedStandTitleTV;
+    private TextView selectedStandStatusTV;
+    private View collapsedView;
+    private DropDownView dropDownView;
+    private View expandedView;
+    private ImageView headerChevronIV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupViews();
+        setupList();
+
+        dropDownView.setHeaderView(collapsedView);
+        dropDownView.setExpandedView(expandedView);
+        dropDownView.setDropDownListener(dropDownListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        headerChevronIV.setRotation(dropDownView.isExpanded()
+                ? 180f : 0f);
+    }
+
+    public void setStandStateWithId(String waitTime, int standId) {
+        if (standId >= 0 && standId < NUM_OF_STANDS) {
+            waitTimes[standId] = waitTime;
+            adapter.notifyItemChanged(standId);
+        }
+
+        // Should update currently selected stand wait time as well
+        if (selectedStandId == standId) {
+            selectedStandStatusTV.setText(waitTime);
+        }
+    }
+
+    private void setupList() {
+        waitTimes = new String[] {"3 minute wait", "Closed", "No wait time", "10 minute wait"};
+        viewActions.setSelectedStand(1);
+        adapter = new DropDownAdapter(viewActions);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        for (int i = 0; i < waitTimes.length; i++) {
+            setStandStateWithId(waitTimes[i], i);
+        }
+    }
+
+    private void setupViews() {
+        dropDownView = findViewById(R.id.drop_down_view);
+        collapsedView = LayoutInflater.from(this).inflate(R.layout.view_drop_down_header, null, false);
+        expandedView = LayoutInflater.from(this).inflate(R.layout.view_drop_down_expanded, null, false);
+
+        selectedStandTitleTV = collapsedView.findViewById(R.id.selected_stand_title);
+        selectedStandStatusTV = collapsedView.findViewById(R.id.selected_stand_status);
+        recyclerView = expandedView.findViewById(R.id.recyclerView);
+        headerChevronIV = collapsedView.findViewById(R.id.chevron_image);
+    }
+
+    private final DropDownView.DropDownListener dropDownListener = new DropDownView.DropDownListener() {
+        @Override
+        public void onExpandDropDown() {
+            adapter.notifyDataSetChanged();
+            ObjectAnimator.ofFloat(headerChevronIV, View.ROTATION.getName(), 180).start();
+        }
+
+        @Override
+        public void onCollapseDropDown() {
+            ObjectAnimator.ofFloat(headerChevronIV, View.ROTATION.getName(), -180, 0).start();
+        }
+    };
+
+    private final DropDownAdapter.ViewActions viewActions = new DropDownAdapter.ViewActions() {
+        @Override
+        public void collapseDropDown() {
+            dropDownView.collapseDropDown();
+        }
+
+        @Override
+        public void setSelectedStand(int standId) {
+            selectedStandTitleTV.setText(getStandTitle(standId ));
+            selectedStandStatusTV.setText(getStandStatus(standId));
+            selectedStandId = standId;
+        }
+
+        @Override
+        public String getStandTitle(int standId) {
+            String title = "";
+            switch (standId) {
+                case 0:
+                    title = getString(R.string.standBrooklynLemonade);
+                    break;
+                case 1:
+                    title = getString(R.string.standManhattanBourjeeSliders);
+                    break;
+                case 2:
+                    title = getString(R.string.standQueensAndCakes);
+                    break;
+                case 3:
+                    title = getString(R.string.standBronxTea);
+                    break;
+            }
+            return title;
+        }
+
+        @Override
+        public String getStandStatus(int standId) {
+            return waitTimes[standId] != null ? waitTimes[standId] : "";
+        }
+
+        @Override
+        public int getSelectedStand() {
+            return selectedStandId;
+        }
+    };
+
+}
